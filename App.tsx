6a71790dacc7b9e3ace655f8de70cce128449f35
crisanger <crisanger@desktop.com>
2021-11-28 16:43:04 -0500

    Feature(navigation) react-navigation-v6 added

--- App.tsx
+++ App.tsx
@@ -1,36 +1,73 @@
 import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { NavigationContainer, DarkTheme, DefaultTheme, } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,  
-  Text,
-  useColorScheme,
-  View,
+  Button,
+  ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View,
 } from 'react-native';
-
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+//---------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  Container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  Title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  Description: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+type RootStackParams = {
+  Home: undefined;
+  Details: { movieId: number } | undefined;
+};
+
+type HomeProps = NativeStackScreenProps<RootStackParams, 'Home'>;
+const HomeScreen: React.FC<HomeProps> = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={styles.Title}>Hola</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details',{ movieId: 69 })}
+      />
+    </View>
+  );
+}
+type DetailsProps = NativeStackScreenProps<RootStackParams, 'Details'>;
+const DetailsScreen: React.FC<DetailsProps> = ({ route }) => {
+  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Mundo: {route.params?.movieId}</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  
+  const isDarkMode = useColorScheme() === 'dark';
+  const Stack = createStackNavigator<RootStackParams>();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Text>Hola Mundo</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
